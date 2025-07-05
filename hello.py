779f86452e7b5f4c39862723471db0fc7f2cd33f
pychang <pychang@nksut.edu.tw>
2025-07-05 13:35:26 +0800

    first commit

---
+++ hello.py
@@ -0,0 +1,11 @@
+# ctrl + ,
+# 1. script
+# 2. interactive
+# 3. notebook, google colab
+# This is a simple Python script that prints "Hello, World!" to the console. 
+x= 1
+y=2
+z= x + y
+print("Hello, World!")
+print(z)
+
---
+++ notes.md
@@ -0,0 +1,15 @@
+conda create -n myenv2025 python=3.10
+conda create -n myenv2026 python=3.10
+base 
+venv01 pyhton=3.11
+venv02 pyhton=3.08
+ctrl + shfit + P
+
+add .
+status
+commit
+log --oneline
+
+push
+pull
+
